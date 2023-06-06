595dd808c59ecb008a91226f068c42b528e7339f
Warren Galyen <wgalyen@hotmail.com>
2023-06-06 11:22:07 -0700

    wip: credentials auth flow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { type AppType } from "next/app";
+import { AppProps, type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
@@ -6,15 +6,18 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-                                                       Component,
-                                                       pageProps: { session, ...pageProps },
-                                                     }) => {
-  return (
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-  );
+const MyApp: any = ({
+                        // AppType<{ session: Session | null }>
+                        Component,
+                        pageProps: { session, ...pageProps },
+                    }: AppProps) => {
+    const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page);
+
+    return getLayout(
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 };
 
 export default api.withTRPC(MyApp);
