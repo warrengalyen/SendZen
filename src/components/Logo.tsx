08780a64f724713fbb506d6ca5f8558f56db90b3
Warren Galyen <wgalyen@hotmail.com>
2023-08-01 09:44:25 -0700

    feat: initial layout for campaign editor

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,15 +4,21 @@ import Link from "next/link";
 export default function Logo({
   type,
   justifyContent,
+  colorTheme,
 }: {
   justifyContent: string;
   type?: "iconAndText";
+  colorTheme?: "light" | "dark";
 }) {
   return (
     <Link href="/">
       <div className={`${justifyContent} align-center flex`}>
         {type === "iconAndText" && (
-          <h3 className="tracking-loose mb-0 pb-0 text-2xl font-bold text-gray-900">
+          <h3
+            className={`${
+              colorTheme === "light" ? "text-white" : "text-gray-900"
+            } tracking-loose mb-0 pb-0 text-2xl font-bold`}
+          >
             SendZen
           </h3>
         )}
