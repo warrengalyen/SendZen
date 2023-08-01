c53809fbfcf4eaa044cd24b301db018c111b6ffa
Warren Galyen <wgalyen@hotmail.com>
2023-07-31 18:12:24 -0700

    wip: tab styling campaignid page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html className="h-full">
-            <Head />
-            <body className="h-full">
-            <Main />
-            <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html className="h-full">
+      <Head />
+      <body className="h-full bg-gray-50">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
