166056f7f63b92d5b553367554e8723c9985e68b
Warren Galyen <wgalyen@hotmail.com>
2023-07-31 18:04:00 -0700

    feat: campaign creation flow

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -3,34 +3,34 @@ import bcrypt from "bcrypt";
 import { z } from "zod";
 
 import {
-    createTRPCRouter,
-    publicProcedure,
-    protectedProcedure,
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
 } from "~/server/api/trpc";
 
 export const authRouter = createTRPCRouter({
-    createAccount: publicProcedure
-        .input(
-            z.object({
-                firstName: z.string(),
-                lastName: z.string(),
-                email: z.string(),
-                password: z.string(),
-            })
-        )
-        .mutation(({ ctx, input }) => {
-            const hashedPassword = bcrypt.hashSync(input.password, 10);
-            return ctx.prisma.user
-                .create({
-                    data: {
-                        first_name: input.firstName,
-                        last_name: input.lastName,
-                        email: input.email,
-                        password: hashedPassword,
-                        send_from_name: input.firstName + " " + input.lastName,
-                    },
-                })
-                .then(() => true)
-                .catch(() => new TRPCError({ code: "INTERNAL_SERVER_ERROR" }));
-        }),
+  createAccount: publicProcedure
+    .input(
+      z.object({
+        firstName: z.string(),
+        lastName: z.string(),
+        email: z.string(),
+        password: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const hashedPassword = bcrypt.hashSync(input.password, 10);
+      return await ctx.prisma.user
+        .create({
+          data: {
+            first_name: input.firstName,
+            last_name: input.lastName,
+            email: input.email,
+            password: hashedPassword,
+            send_from_name: input.firstName + " " + input.lastName,
+          },
+        })
+        .then(() => true)
+        .catch(() => new TRPCError({ code: "INTERNAL_SERVER_ERROR" }));
+    }),
 });
